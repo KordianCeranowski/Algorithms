c2e2449eab629a5810490daaa56a80404f212ea1
KordianCeranowski <KordianCeranowski@users.noreply.github.com>
2019-05-21 16:42:27 +0200

    patternSearch

---
+++ PatternSearch/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        ReadFromFile test = new ReadFromFile();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj nazwę pliku TEXT");
+        String T = test.getText(scanner.next() + ".txt");
+        System.out.println("Podaj nazwę pliku PATTERN");
+        String P = test.getText(scanner.next() + ".txt");
+
+        Naive.naive(P,T);
+        RabinKarp.rabinKarp(P, T);
+        KnuthMorrisPratt.KMP(P, T);
+    }
+
+
+}
